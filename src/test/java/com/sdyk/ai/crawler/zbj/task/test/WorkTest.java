53cd8b07a13cd4d82e6d7fa4b80edcb4bb67906a
gcy <gcy116149@163.com>
2018-03-08 20:48:50 +0800

    添加 BasicRequester 测试方法

--- src/test/java/com/sdyk/ai/crawler/zbj/task/test/WorkTest.java
+++ src/test/java/com/sdyk/ai/crawler/zbj/task/test/WorkTest.java
@@ -1,10 +1,13 @@
 package com.sdyk.ai.crawler.zbj.task.test;
 
-import com.sdyk.ai.crawler.zbj.task.Task;
 import com.sdyk.ai.crawler.zbj.task.WorkScanTask;
 import org.junit.Test;
+import org.tfelab.io.requester.BasicRequester;
+import org.tfelab.io.requester.Task;
 import org.tfelab.io.requester.chrome.ChromeDriverAgent;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -19,20 +22,35 @@ public class WorkTest {
 
 		ChromeDriverAgent agent = new ChromeDriverAgent();
 
-		Queue<Task> queue = new LinkedBlockingQueue<>();
+		Queue<com.sdyk.ai.crawler.zbj.task.Task> queue = new LinkedBlockingQueue<>();
 
 		queue.add(WorkScanTask.generateTask("http://shop.zbj.com/16123923/", 1));
 
 		while(!queue.isEmpty()) {
 
-			Task t = queue.poll();
+			com.sdyk.ai.crawler.zbj.task.Task t = queue.poll();
 
 			agent.fetch(t);
 
-			for (Task task : t.postProc(agent.getDriver())) {
+			for (com.sdyk.ai.crawler.zbj.task.Task task : t.postProc(agent.getDriver())) {
 				queue.add(task);
 			}
-
 		}
+	}
+
+	@Test
+	public void testBasicRequester() {
+		Task t = null;
+		try {
+			t = new Task("http://www.baidu.com");
+			BasicRequester.getInstance().fetch(t);
+			t.setDelete();
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
 	}
  }
