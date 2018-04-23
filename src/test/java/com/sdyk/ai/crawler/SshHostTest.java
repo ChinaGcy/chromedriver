958668106fc025552f7f1bfb28f4a4a9447bad52
karajan <karajan@tfelab.org>
2018-04-23 22:59:20 +0800

    增加使用私钥登录服务器的测试用例

---
+++ src/test/java/com/sdyk/ai/crawler/SshHostTest.java
@@ -0,0 +1,21 @@
+package com.sdyk.ai.crawler;
+
+import one.rewind.io.SshManager;
+import org.junit.Test;
+
+import java.io.File;
+
+public class SshHostTest {
+
+	@Test
+	public void test() throws Exception {
+
+		File privateKey = new File("docker.pem");
+
+		SshManager.Host ssh_host = new SshManager.Host("10.0.0.56", 22, "root", privateKey);
+
+		ssh_host.connect();
+
+		System.err.println(ssh_host.exec("ps aux"));
+	}
+}
